03d13f2711758d390cd72181512a3f77debfd0e6
Eyaya <eyayab21@gmail.com>
2024-02-24 22:54:39 +0300

    evaluating the different retrievers

---
+++ RAG/rag_evaluation.py
@@ -0,0 +1,80 @@
+from langchain_community.document_loaders import DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import Document
+from langchain_openai import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
+from langchain_community.document_loaders import Docx2txtLoader
+from langchain.prompts import ChatPromptTemplate,SystemMessagePromptTemplate,HumanMessagePromptTemplate
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from operator import itemgetter
+
+from langchain.chat_models import ChatOpenAI
+from langchain.schema.output_parser import StrOutputParser
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+from langchain.output_parsers import ResponseSchema
+from langchain.output_parsers import StructuredOutputParser
+from tqdm import tqdm
+import pandas as pd
+from datasets import Dataset
+import os
+import chromadb
+import shutil
+import re
+from dotenv import load_dotenv
+load_dotenv()
+
+
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+
+import sys
+sys.path.append('../')
+from tqdm import tqdm
+import pandas as pd
+
+from ragas.metrics import (
+    answer_relevancy,
+    faithfulness,
+    context_recall,
+    context_precision,
+    context_relevancy,
+    answer_correctness,
+    answer_similarity
+)
+
+from ragas.metrics.critique import harmfulness
+from ragas import evaluate
+
+def create_ragas_dataset(rag_pipeline, eval_dataset):
+  rag_dataset = []
+  for row in tqdm(eval_dataset):
+    answer = rag_pipeline.invoke({"question" : row["question"]})
+    rag_dataset.append(
+        {"question" : row["question"],
+         "answer" : answer["response"].content,
+         "contexts" : [context.page_content for context in answer["context"]],
+         "ground_truths" : [row["ground_truth"]]
+         }
+    )
+  rag_df = pd.DataFrame(rag_dataset)
+  rag_eval_dataset = Dataset.from_pandas(rag_df)
+  return rag_eval_dataset
+
+def evaluate_ragas_dataset(ragas_dataset):
+  result = evaluate(
+    ragas_dataset,
+    metrics=[
+        context_precision,
+        faithfulness,
+        answer_relevancy,
+        context_recall,
+        context_relevancy,
+        answer_correctness,
+        answer_similarity
+    ],
+  )
+  return result
+
+
